98500d623d66b69e0461cebb299d7bbd3765c8ac
Amsit S <dezavue3@gmail.com>
2022-04-01 16:31:24 +0700

    Update detect.lua

--- .github/workflows/detect.lua
+++ .github/workflows/detect.lua
@@ -2,5 +2,20 @@ package.path = package.path .. ';.\\.github\\workflows\\?.lua;'
 
 
 local sysdetect = require "sysdetect"
-print( sysdetect.detect() )
-print()
+local sys, arch = sysdetect.detect()
+if sys == "windows" the
+   local popen = io.popen
+   local close = io.close
+   local output = io.output
+   local wrife = io.write
+
+   local p = popen('powershell Get-ChildItem -Path .github\\workflows\\* -Includr *.yml')
+   for l in p:lines() do
+      print(l)
+   end
+
+   local rc = { p:close() }
+   for k, v in pairs(rc) do
+      print('rc', k, v)
+   end
+end
