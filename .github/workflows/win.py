07888c9f8ccdc8671ca4d6229d7ff4080873854a
Amsit S <dezavue3@gmail.com>
2022-05-09 12:00:31 +0700

    Update win.py

--- .github/workflows/win.py
+++ .github/workflows/win.py
@@ -1,7 +1,11 @@
 from sys import platform, exec_prefix, base_prefix
-
+from os import environ
 if __name__ == '__main__':
-   print(platform)
-   print(base_prefix)
-   print(exec_prefix)
+   print('paltform', platform)
+   print('base_prefix', base_prefix)
+   print('exec_prefix', exec_prefix)
+   print('environments:"
+   for key, val in environ.items():
+      print('  ', key, val)
+   
    
