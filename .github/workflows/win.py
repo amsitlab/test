60de8c5adf1b28d9029e5261df96ac075ffeadee
Amsit S <dezavue3@gmail.com>
2022-05-09 12:01:47 +0700

    Update win.py

--- .github/workflows/win.py
+++ .github/workflows/win.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
    print('paltform', platform)
    print('base_prefix', base_prefix)
    print('exec_prefix', exec_prefix)
-   print('environments:"
+   print('environments:')
    for key, val in environ.items():
       print('  ', key, val)
    
